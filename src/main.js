00887c3dad6392453a36291ed0be9f5dc09255a2
Umesh Thapa <39909267+umeshthapa12@users.noreply.github.com>
2021-10-01 11:20:47 +0545

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import VueMq from "vue-mq";
 
+Vue.use(VueMq, {
+  breakpoints: {
+    mobile: 576,
+    tablet: 768,
+    ipad: 992,
+    laptop: 1200,
+    desktop: 1600,
+    xlscreen: Infinity,
+  },
+});
 Vue.config.productionTip = false
 
 new Vue({
